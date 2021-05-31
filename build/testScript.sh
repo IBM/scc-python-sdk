00e1e214edeb3d9410840c48fa7954374a135601
Gaurav Goswami <gaurav.goswami2@ibm.com>
2021-05-31 19:24:45 +0530

    testScript fixed

--- build/testScript.sh
+++ build/testScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $TRAVIS_BRANCH == "main" && $TRAVIS_PULL_REQUEST == "false" ]; then
+if [[ $TRAVIS_BRANCH == "main" && $TRAVIS_PULL_REQUEST == "false" ]]; then
   curl https://us-south.functions.appdomain.cloud/api/v1/web/e6b54af6-ab44-4149-a8e4-e906dcc58136/default/secadvstg-location-shift.json
   echo "${FINDINGS_ENV}" | base64 -d >> findings_v1.env
   echo "${NOTIFICATIONS_ENV}" | base64 -d >> notifications_v1.env
