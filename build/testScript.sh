970acaeb0b081918d283df0cfad27af30e67e8db
Gaurav Goswami <gaurav.goswami2@ibm.com>
2021-06-01 14:44:03 +0530

    fix(SDK): IBM Cloud SCC Python SDK

--- build/testScript.sh
+++ build/testScript.sh
@@ -5,6 +5,5 @@ if [[ $TRAVIS_BRANCH == "main" && $TRAVIS_PULL_REQUEST == "false" ]]; then
   echo "${FINDINGS_ENV}" | base64 -d >> findings_v1.env
   echo "${NOTIFICATIONS_ENV}" | base64 -d >> notifications_v1.env
   # echo "${CONFIGURATION_GOVERNANCE_ENV}" | base64 -d >> configuration_governance_v1.env
-  tox
-  find . -name "*.env" -type f -delete
+  pytest test/integration
 fi
