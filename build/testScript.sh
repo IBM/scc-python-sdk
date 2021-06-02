b6cd3ab142ea8628af4ccf0cd175449459c79b7a
Gaurav Goswami <gaurav.goswami2@ibm.com>
2021-06-02 13:39:11 +0530

    fix(TestScript): ITs script should exit with error code for test failure

--- build/testScript.sh
+++ build/testScript.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 if [[ $TRAVIS_BRANCH == "main" && $TRAVIS_PULL_REQUEST == "false" ]]; then
   curl https://us-south.functions.appdomain.cloud/api/v1/web/e6b54af6-ab44-4149-a8e4-e906dcc58136/default/secadvstg-location-shift.json
   echo "${FINDINGS_ENV}" | base64 -d >> findings_v1.env
