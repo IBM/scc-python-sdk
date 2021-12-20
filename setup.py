37c6dcf7e5d3e651b9fe8a6cfffa8ed221d8128c
scccomm <scccomm@in.ibm.com>
2021-12-20 07:40:30 +0000

    Update version 1.1.2 -> 2.0.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import os
 import sys
 import pkg_resources
 
-__version__ = '1.1.2'
+__version__ = '2.0.0'
 PACKAGE_NAME = 'ibm_scc'
 PACKAGE_DESC = 'Python client library for IBM Cloud Security & Compliance Center Services'
 
