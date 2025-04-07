9ad34b0ff934a84829adf6f32567fa26f07f7041
Bryan Bosely <bbosely@us.ibm.com>
2025-04-07 13:08:26 -0400

    chore(SCC): Manual change integration test test_replace_target
    Signed-off-by: Bryan Bosely <bbosely@us.ibm.com>

--- test/integration/test_security_and_compliance_center_api_v3.py
+++ test/integration/test_security_and_compliance_center_api_v3.py
@@ -1041,52 +1041,12 @@ class TestSecurityAndComplianceCenterApiV3:
 
     @needscredentials
     def test_replace_target(self):
-        # Construct a dict representation of a Account model
-        account_model = {
-            'id': '531fc3e28bfc43c5a2cea07786d93f5c',
-            'name': 'NIST',
-            'type': 'account_type',
-        }
-        # Construct a dict representation of a Tags model
-        tags_model = {
-            'user': ['testString'],
-            'access': ['testString'],
-            'service': ['testString'],
-        }
-        # Construct a dict representation of a Resource model
-        resource_model = {
-            'report_id': '30b434b3-cb08-4845-af10-7a8fc682b6a8',
-            'home_account_id': '2411ffdc16844b07b42521c3443f456d',
-            'id': 'crn:v1:bluemix:public:kms:us-south:a/5af747ca19a8a278b1b6e4eec20df507:03502a50-4ea9-463c-80e5-e27ed838cdb6::',
-            'resource_name': 'jeff\'s key',
-            'account': account_model,
-            'component_id': 'cloud-object_storage',
-            'component_name': 'cloud-object_storage',
-            'environment': 'ibm cloud',
-            'tags': tags_model,
-            'status': 'compliant',
-            'total_count': 140,
-            'pass_count': 123,
-            'failure_count': 12,
-            'error_count': 5,
-            'skipped_count': 7,
-            'completed_count': 135,
-            'service_name': 'pm-20',
-            'instance_crn': 'testString',
-        }
-        # Construct a dict representation of a Credential model
-        credential_model = {
-            'secret_crn': 'dummy',
-            'resources': [resource_model],
-        }
-
         response = self.security_and_compliance_center_api_service.replace_target(
             instance_id='acd7032c-15a3-484f-bf5b-67d41534d940',
             target_id=target_id_link,
-            account_id='be200c80cabc456e91139e4152327823',
-            trusted_profile_id='Profile-a0a4c149-4fed-47ff-bfb2-680bcfaa64d3',
-            name='Sample Target Name',
-            credentials=[credential_model],
+            account_id='62ecf99b240144dea9125666249edfcb',
+            trusted_profile_id='Profile-cb2c1829-9a8d-4218-b9cd-9f83fc814e54',
+            name='Updated Target Name',
         )
 
         assert response.get_status_code() == 200
